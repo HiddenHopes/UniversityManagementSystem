4136a0543d454ad3ec5fe2b6b54e2808b5afbf34
Khurshedul Alam <92214287+Khurshedul51@users.noreply.github.com>
2021-12-25 20:56:04 -0800

    C201051

--- src/DBUTILS.java
+++ src/DBUTILS.java
@@ -6,13 +6,13 @@ import java.sql.Statement;
 
 public class DBUTILS {
 
-    private static final String jdbcURL = "jdbc:mysql://localhost:3306/studentdb";
+    /*private static final String jdbcURL = "jdbc:mysql://localhost:3306/studentdb";
     private static final String jdbcUsername = "root";
-    private static final String jdbcPassword = "root";
+    private static final String jdbcPassword = "root";*/
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
-        Class.forName("com.mysql.jdbc.Driver"); 
-        Connection connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        Class.forName("org.sqlite.JDBC"); 
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:studentdb.db");
         return connection;
     }
 
