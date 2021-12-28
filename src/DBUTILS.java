46c9a22e25a163265ad80942d47363d9559e329c
M. Main <m.main1010@gmail.com>
2021-12-28 20:42:02 +0600

    commited by main

--- src/DBUTILS.java
+++ src/DBUTILS.java
@@ -6,9 +6,9 @@ import java.sql.Statement;
 
 public class DBUTILS {
 
-    private static final String jdbcURL = "jdbc:mysql://localhost:3306/studentdb";
+    private static final String jdbcURL = "jdbc:mysql://localhost:3306/mainpro";
     private static final String jdbcUsername = "root";
-    private static final String jdbcPassword = "root";
+    private static final String jdbcPassword = "m97727247";
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
         Class.forName("com.mysql.jdbc.Driver"); 
